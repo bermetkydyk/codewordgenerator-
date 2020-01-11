87d4a0019fbe1713f02e40d635be21b3e9d8110e
bermetkydyk <38230942+bermetkydyk@users.noreply.github.com>
2020-01-10 23:30:42 -0500

    Create codewordgenerator.hpp

---
+++ codewordgenerator.hpp
@@ -0,0 +1,23 @@
+#ifndef CODEWORD_GENERATOR_
+#define CODEWORD_GENERATOR_
+#include <iostream>
+#include <queue>
+#include <regex>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+// this impelementation will only have two public functions 
+class CodewordGenerator{
+public:
+    string generateShortestWord(string pattern);
+    string generateLengthWord(string pattern, int length);
+
+
+private:
+
+
+};
+
+#endif 
